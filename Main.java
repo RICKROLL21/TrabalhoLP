26fb037fc8308494cda9c0f1fbc439fcb8a75298
RICKROLL21 <52740416+RICKROLL21@users.noreply.github.com>
2020-06-15 20:00:07 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,15 +4,17 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	
-		Estrutura es = new Estrutura();
-		es.menuInicial();
-		
-		
 		Elemento el= new Elemento();
 		el.getElementoRecursiveFilhos(el, 0);
 		
 		Elemento ele= new Elemento();
 		ele.getElementoRecursiveFilhos(ele, 0);
+		
+		Estrutura es = new Estrutura();
+		es.menuInicial();
+		
+		
+		
 		
 		
 		
